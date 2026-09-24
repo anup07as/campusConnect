34a0344ed873bbdc0fe8bd9c146ba48af6414df2
anup07as <anupkumar015as@gmail.com>
2026-09-25 00:11:53 +0530

    Fix CampusAI authentication

--- script.js
+++ script.js
@@ -503,9 +503,10 @@ function sendCampusAIMessage() {
 
         method: "POST",
 
-        headers: {
-            "Content-Type": "application/json"
-        },
+       headers: {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + localStorage.getItem("token")
+},
 
         body: JSON.stringify({
             history: window.campusAIHistory
